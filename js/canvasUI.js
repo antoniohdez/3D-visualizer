2d7ed37c223bae547348b566d80583feae31bd0d
Antonio <antonio.hdez93@gmail.com>
2015-04-26 23:15:07 -0500

    UI changes and events handlers. - UI style for rotation menu. - Event handlers for rotation menu (rotate object on Y axis).

--- js/canvasUI.js
+++ js/canvasUI.js
@@ -2,8 +2,6 @@
 	General UI elements
 */
 var rotationMenu = document.createElement("div");
-var leftRotationButton = document.createElement("button");
-var rightRotationButton = document.createElement("button");
 var icon;
 
 /*
@@ -16,19 +14,31 @@ rotationMenu.className = "rotationMenu";
 	Left rotation button
 */
 
+var leftRotationButton = document.createElement("button");
+
 icon = document.createElement("i");
 icon.className = "fa fa-chevron-left";
 
 leftRotationButton.appendChild( icon );
 
+leftRotationButton.addEventListener("mousedown", leftRotationDown);
+leftRotationButton.addEventListener("mouseup", leftRotationUp);
+leftRotationButton.addEventListener("mouseleave", leftRotationUp);
+
 /*
 	Right rotation button
 */
+
+var rightRotationButton = document.createElement("button");
 
 icon = document.createElement("i");
 icon.className = "fa fa-chevron-right";
 
 rightRotationButton.appendChild( icon );
+
+rightRotationButton.addEventListener("mousedown", rightRotationDown);
+rightRotationButton.addEventListener("mouseup", rightRotationUp);
+rightRotationButton.addEventListener("mouseleave", rightRotationUp);
 
 /*
 	Add button to rotation menu
---
+++ js/eventHandlers.js
@@ -0,0 +1,25 @@
+var rotateCamera = function (angle){
+	rubik.rotation.y += angle*(Math.PI/180);
+}
+
+var interval;
+
+var leftRotationDown = function (){
+	interval = setInterval(function (){
+		rotateCamera(1);
+	}, 25);
+}
+
+var leftRotationUp = function (){
+	clearInterval(interval);
+}
+
+var rightRotationDown = function (){
+	interval = setInterval(function (){
+		rotateCamera(-1);
+	}, 25);
+}
+
+var rightRotationUp = function (){
+	clearInterval(interval);
+}
