fbd896041b2068390e9fc3d026323cc87b304530
Antonio <antonio.hdez93@gmail.com>
2015-05-25 23:29:14 -0500

    PHP script changed to unzip files. - Basic implementation to unzip files.

--- uploads_handler.php
+++ uploads_handler.php
@@ -1,17 +1,30 @@
 <?php
 
-if( $_SERVER['REQUEST_METHOD'] === 'POST' && $_FILES["obj_file"]["error"] === 0 ){
+if( $_SERVER['REQUEST_METHOD'] === 'POST' && $_FILES["zip_file"]["error"] === 0 ){
 
 	$token = bin2hex(openssl_random_pseudo_bytes(16));
 
 	// Get file extension
-	$ext = strtolower(pathinfo($_FILES["obj_file"]["name"], PATHINFO_EXTENSION));
+	$ext = strtolower(pathinfo($_FILES["zip_file"]["name"], PATHINFO_EXTENSION));
 
+	$path = __DIR__ . "/uploads/" . $token;
 	// Check if file extension is valid and if folder can be created
-	if( $ext === "obj"  &&  mkdir( "uploads/" . $token ) ){
+	if( $ext === "zip"  &&  mkdir( $path ) ){
+		
+		chdir($path);
+		mkdir("obj");
 
-		if( move_uploaded_file( $_FILES["obj_file"]["tmp_name"], "uploads/" . $token . "/" . $_FILES['obj_file']['name']) ){
+		if( move_uploaded_file( $_FILES["zip_file"]["tmp_name"], "file.zip" ) ){
 			
+			$zip = new ZipArchive;
+			if ($zip->open("file.zip") === TRUE) {
+				$zip->extractTo('/obj');
+				$zip->close();
+				echo 'ok';
+			} else {
+				echo 'failed';
+			}
+
 			echo $token;
 			exit();
 
