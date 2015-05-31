0bf65f9e672a0547421ce7e8cf291f42c62cd9ce
Antonio <antonio.hdez93@gmail.com>
2015-05-30 23:26:48 -0500

    uploads_handler refactoring and server response format - Functions to handle file upload and unzip. - Json reponse from server. - Minor changes.

--- uploads_handler.php
+++ uploads_handler.php
@@ -1,13 +1,39 @@
 <?php
 
-function find_file($ext){
-	$it = new RecursiveDirectoryIterator("obj/");
-	$display = Array ($ext);
+/*
+	$token => Path where zip will be uploaded
+*/
+function upload_zip($token, $zip_file){
+	$flag = false;
+	// Get file extension
+	$ext = strtolower(pathinfo($_FILES["zip_file"]["name"], PATHINFO_EXTENSION));
+
+	// Path where zip will be uploaded
+	$path = __DIR__ . "/uploads/" . $token;
+
+	if( $ext === "zip"  &&  mkdir( $path ) ){
+		
+		chdir($path);
+
+		if( move_uploaded_file( $_FILES["zip_file"]["tmp_name"], $zip_file ) ){
+			$flag = true;
+		}
+	}
+	return $flag;
+}
+
+/*
+	$ext => File extension to search
+*/
+function find_file($ext, $dir){
+	$it = new RecursiveDirectoryIterator($dir);
+	$display = array($ext);
 	$path = NULL;
 	foreach(new RecursiveIteratorIterator($it) as $file)
 	{
-		if( in_array( strtolower( array_pop( explode( '.', $file ) ) ), $display) ){
-			
+
+		if( in_array( strtolower( array_pop( explode( ".", $file ) ) ), $display) ){
+				
 			if ( is_null( $path ) ) {
 				$path = $file;
 			}// If already found return NULL (not multiple .obj files allowed)
@@ -16,53 +42,69 @@ function find_file($ext){
 			}
 		}
 	}
-	return $path;
+	return (string)$path;
 }
 
-if( $_SERVER['REQUEST_METHOD'] === 'POST' && $_FILES["zip_file"]["error"] === 0 ){
+/*
+	$filename => Path to .zip file
+	$extractDir => Path where zip will be extracted
+*/
+function unzip_file($filename, $extractDir){
+	$zip = new ZipArchive;
+	$flag = $zip->open( $filename );
+	
+	if ( $flag ) {
+		mkdir($extractDir);
+		$flag = $zip->extractTo( $extractDir );
+		$zip->close();
+	} 
+	
+	return $flag;
+}
+
+if( $_SERVER["REQUEST_METHOD"] === "POST" && $_FILES["zip_file"]["error"] === 0 ){
 
 	$token = bin2hex(openssl_random_pseudo_bytes(16));
+	$zip_file = "file.zip";
 
-	// Get file extension
-	$ext = strtolower(pathinfo($_FILES["zip_file"]["name"], PATHINFO_EXTENSION));
+	if( upload_zip( $token, $zip_file ) ){
 
-	$path = __DIR__ . "/uploads/" . $token;
-	// Check if file extension is valid and if folder can be created
-	if( $ext === "zip"  &&  mkdir( $path ) ){
-		
-		chdir($path);
-		mkdir("obj");
-
-		if( move_uploaded_file( $_FILES["zip_file"]["tmp_name"], "file.zip" ) ){
+		$extraction_path = "obj/";
+		if( unzip_file( $zip_file, $extraction_path ) ){
 			
-			$zip = new ZipArchive;
-			if ($zip->open("file.zip") === TRUE) {
-				$flag = $zip->extractTo('obj/');
-				$zip->close();
-
-				// Unzip success
-				if( $flag ){
-					$obj_path = find_file("obj");
-					// Obj not found
-					if( is_null( $obj_path ) ){
-						echo ".obj not found";
-					}else{ // Obj found
-						echo ".obj found: ".$obj_path;
-					}
-
-				}else{ // Unzip error
-					echo "Unzip error"
-				}
-			} else {
-				echo "open file failed";
+			$obj_path = find_file( "obj", $extraction_path );
+			$mtl_path = find_file( "mtl", $extraction_path );
+			
+			// Obj not found
+			if( is_null( $obj_path ) ){
+				echo json_encode( 
+					array(
+						"status" => "fail", 
+						"message" => ".obj file not found"
+					)
+				);
+			}else{ // Obj found
+				echo json_encode( 
+					array(
+						"status" => "success", 
+						"data" => array(
+							"token" => $token,
+							"obj" => $obj_path,
+							"mtl" => $mtl_path
+						)
+					)
+				);
 			}
-
-			echo $token;
-			exit();
-
 		}
-
-	}
+	}else{
+		echo json_encode( 
+			array(
+				"status" => "fail", 
+				"message" => "Invalid file extension"
+			)
+		);
+	}		
+	exit();
 
 }
 
