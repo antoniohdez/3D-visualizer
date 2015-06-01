3f658f97b43f959a07aef17d268963aa2284eed4
Antonio <antonio.hdez93@gmail.com>
2015-06-01 00:16:33 -0500

    This commit fixes #1 issue - New function to find boundings instead of just getting the radius. - Adjustment of the position of the camera to get a better perspective of the model.

--- js/loader.js
+++ js/loader.js
@@ -7,10 +7,12 @@ var cleanScene = function(name){
 	scene.remove(model);
 }
 
-var resetCamera = function(cameraZ){
-	camera.position.set(0, 0, cameraZ);
+var resetCamera = function(cameraY, cameraZ){
+	camera.position.set(0, cameraY, cameraZ);
 	camera.rotation.set(0, 0, 0, 'XYZ');
-	camera.far = cameraZ*2;
+	camera.far = cameraZ * 10;
+	
+	controls.target = new THREE.Vector3(0, cameraY, 0);
 
 	// Must be called after change of parameters.
 	camera.updateProjectionMatrix();
@@ -22,23 +24,55 @@ var resetScene = function(){
 	scene.rotation.set(0, 0, 0, 'XYZ');
 }
 
-var getRadiusGeometry = function(object){
-	var radius = 0;
-	if( object.type === "Mesh" ){
-		return object.geometry.boundingSphere.radius;
-	}else{
-		var tmpRadius = 0;
-		object.children.forEach(function(obj){
-			
-			tmpRadius = getRadiusGeometry(obj);
-			
-			if( tmpRadius > radius ){
-				radius = tmpRadius;
+var compareBounding = function(bounding, tmpBounding){
+	for (var prop in bounding.positive) {
+		if (bounding.positive.hasOwnProperty(prop)) {
+			if( tmpBounding.positive[prop] > bounding.positive[prop] ){
+				bounding.positive[prop] = tmpBounding.positive[prop];
 			}
+		}
+	}
+	for (var prop in bounding.negative) {
+		if (bounding.negative.hasOwnProperty(prop)) {
+			if( tmpBounding.negative[prop] < bounding.negative[prop] ){
+				bounding.negative[prop] = tmpBounding.negative[prop];
+			}	
+		}
+	}
+	return bounding;
+}
+
+var getBoundingGeometry = function(object){
+	var bounding = {
+		positive : { x : 0, y : 0, z : 0 },
+		negative : { x : 0, y : 0, z : 0 }
+	};
+	var radius = 0;
+	var center = 0;
+
+	if( object.type === "Mesh" ){
+		radius = object.geometry.boundingSphere.radius;
+		center = object.geometry.boundingSphere.center;
+
+		bounding.positive.x = center.x + radius;
+		bounding.positive.y = center.y + radius;
+		bounding.positive.z = center.z + radius;
+
+		bounding.negative.x = center.x - radius;
+		bounding.negative.y = center.y - radius;
+		bounding.negative.z = center.z - radius;
+
+	}else{
+		var tmpBounding = bounding;
+		object.children.forEach(function(obj){
+
+			tmpBounding = getBoundingGeometry(obj);
+			bounding = compareBounding(bounding, tmpBounding);
+
 		});
 	}
 
-	return radius;
+	return bounding;
 }
 
 /*
@@ -46,6 +80,7 @@ var getRadiusGeometry = function(object){
 */
 
 var loader = function(url, mtlUrl){
+	// Loaded model is always named "loader-model"
 	cleanScene("loader-model");
 	resetScene();
 
@@ -59,11 +94,14 @@ var loader = function(url, mtlUrl){
 		object.position.z = 0;
 		object.scale.set(1, 1, 1);
 
+		// Loaded model is always named "loader-model"
 		object.name = "loader-model";
+		
+		var bounding = getBoundingGeometry(object);
+		var z = bounding.positive.z*4;
+		var y = ( bounding.positive.y - bounding.negative.y ) / 2;
 
-		var radius = getRadiusGeometry(object);
-		var distance = radius*4;
-		resetCamera( distance );
+		resetCamera(y, z);
 		
 		scene.add(object);
 
